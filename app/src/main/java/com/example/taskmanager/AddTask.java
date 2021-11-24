f29d9d201051b97e801b14492d333e1a3a562e39
bayan-hmaidy98 <bayanhdy98@gmail.com>
2021-11-24 18:55:50 +0200

    finish the lab

--- app/src/main/java/com/example/taskmanager/AddTask.java
+++ app/src/main/java/com/example/taskmanager/AddTask.java
@@ -2,13 +2,15 @@ package com.example.taskmanager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
+
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.generated.model.Task;
 
 public class AddTask extends AppCompatActivity {
 
@@ -16,6 +18,9 @@ public class AddTask extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Button submitTask = (Button) findViewById(R.id.submitTask);
@@ -30,12 +35,26 @@ public class AddTask extends AppCompatActivity {
                 String titles = addTitle.getText().toString();
                 String states = addState.getText().toString();
                 String bodies = addBody.getText().toString();
-                Task addTask = new Task(titles, bodies, State.NEW);
-                Toast.makeText(getApplicationContext(), "Submitted!", Toast.LENGTH_LONG).show();
-                TaskDB myDB = Room.databaseBuilder(getApplicationContext(), TaskDB.class, "tasksDatabase").allowMainThreadQueries().fallbackToDestructiveMigration().build();
-                TaskDAO taskDao = myDB.taskDAO();
 
-                taskDao.save(addTask);
+                Task task = Task.builder()
+                        .title(titles)
+                        .body(bodies)
+                        .state("NEW")
+                        .build();
+
+                Amplify.DataStore.save(
+                        task,
+                        success -> Log.i("Amplify", "Saved item: " + success.item().getId()),
+                        error -> Log.e("Amplify", "Could not save item to DataStore", error)
+                );
+
+//
+//                Task addTask = new Task(titles, bodies, State.NEW);
+//                Toast.makeText(getApplicationContext(), "Submitted!", Toast.LENGTH_LONG).show();
+//                TaskDB myDB = Room.databaseBuilder(getApplicationContext(), TaskDB.class, "tasksDatabase").allowMainThreadQueries().fallbackToDestructiveMigration().build();
+//                TaskDAO taskDao = myDB.taskDAO();
+//
+//                taskDao.save(addTask);
                 Intent addIntent = new Intent(AddTask.this, MainActivity.class);
                 startActivity(addIntent);
 
